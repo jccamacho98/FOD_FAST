3943f1c103d7b12d928f8daab13d9d8127e4234d
jccamacho98 <107131593+jccamacho98@users.noreply.github.com>
2022-12-19 00:03:30 -0500

    version 3

--- app/src/main/java/edu/unicauca/ttm/appcomidas/FragmentRegistrarse.java
+++ app/src/main/java/edu/unicauca/ttm/appcomidas/FragmentRegistrarse.java
@@ -1,5 +1,6 @@
 package edu.unicauca.ttm.appcomidas;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +25,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentRegistrarse extends Fragment {
-
+    TextView tv_date;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -46,13 +57,17 @@ public class FragmentRegistrarse extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //tv_date.findViewById(R.id.FechaNacimientoRegistro);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+     ////////////////////////////////////////////////////////////////////////////////7
+
     }
 
     @Override
@@ -60,5 +75,24 @@ public class FragmentRegistrarse extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_frag_registrarse, container, false);
+
+    }
+
+
+    public void onClick(View view) {
+
+  Calendar cal= Calendar.getInstance();
+  int anio= cal.get(Calendar.YEAR);
+  int mes = cal.get(Calendar.MONTH);
+  int dia = cal.get(Calendar.DAY_OF_MONTH);
+
+  DatePickerDialog dpd = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+
+      @Override
+      public void onDateSet(DatePicker datePicker, int year, int month, int dayofMonth) {
+      String fecha=dayofMonth+"/"+month+"/"+year;
+      }
+  },anio,mes,dia);
+   dpd.show();
     }
 }
--- app/src/main/java/edu/unicauca/ttm/appcomidas/MainActivity.java
+++ app/src/main/java/edu/unicauca/ttm/appcomidas/MainActivity.java
@@ -6,14 +6,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper v_flipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int imagenesPromociones[]=
+                {
+                        R.drawable.promo_salchipapa,
+                        R.drawable.promo_sandwich,
+                        R.drawable.promohambuerguesa};
+        v_flipper =findViewById(R.id.v_flipper);
+
+        for(int imagen: imagenesPromociones){
+            flipperImages(imagen);
+
+        }
+
     }
+
+    //metodo para el visor de imagenes
+    public void flipperImages(int imagePromo){
+        ImageView imageView=new ImageView(this);
+        imageView.setBackgroundResource(imagePromo);
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(6000); //tiempo de la trancision
+        v_flipper.setAutoStart(true);
+        v_flipper.setInAnimation(this, android.R.anim.slide_out_right);
+        v_flipper.setInAnimation(this,android.R.anim.slide_in_left);
+
+    }
+
 
     public void onClick(View view){
 
